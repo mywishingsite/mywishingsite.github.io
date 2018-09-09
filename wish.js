bba3151a39908d7708af4538f120c08f024e67d9
itzSaan <42498676+itzSaan@users.noreply.github.com>
2018-09-09 17:33:06 +0530

    Add files via upload

---
+++ wish.js
@@ -0,0 +1,10 @@
+var e = document.getElementById('saan');
+console.log(e);
+/*                    var a=prompt("Please Enter Your Name\n\n👇👇👇👇👇");
+                    document.write(a);
+*/
+
+function wish(){
+	var a = prompt('Enter Your Name');
+
+}
